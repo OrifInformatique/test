49ebfcb8d746714b1af9e805267f15ab1e3d01db
Tristan Schorr <ScTr>
2024-05-27 10:35:35 +0200

    Creation jeu
    Transformation de l'animation du bonhomme en un jeu d'obstacle en 2D.

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
 // Déplacement du bonhomme ASCII
-let bonhommePosition = -100; // Position initiale à gauche de la fenêtre
+let bonhommeXPos = -100; // Position initiale à gauche de la fenêtre
 const bonhommeElement = document.getElementById("bonhomme");
 
-function moveBonhomme() {
-    bonhommePosition += 10; // Déplace le bonhomme de 10 pixels à chaque fois
-    bonhommeElement.style.left = bonhommePosition + "px";
+let obstacleXPos = 1800; // Position initiale à droite de la fenêtre
+const obstacleElement = document.getElementById("obstacle");
+
+let bonhommeYPos = 440; // Position initiale sur l'axe vertical
+
+let obstacleYPos = 450; // Position initiale sur l'axe vertical
+
+function moveElements() {
+    bonhommeXPos += 10; // Déplace le bonhomme de 10 pixels à chaque fois
+    bonhommeElement.style.left = bonhommeXPos + "px";
+
+    obstacleXPos -= 10; // Déplace l'obstacle de 10 pixels à chaque fois
+    obstacleElement.style.left = obstacleXPos + "px";
 
     // Réinitialise la position lorsque le bonhomme atteint la fin de la page
-    if (bonhommePosition > window.innerWidth) {
-        bonhommePosition = -100;
+    if (bonhommeXPos > window.innerWidth) {
+        bonhommeXPos = -100;
+    }
+    // Réinitialise la position lorsque l'obstacle atteint la fin de la page
+    if (obstacleXPos < -100) {
+        obstacleXPos = 1800;
+    }
+    // Calcule la différence de distance sur l'axe vertical et horizontal pour savoir si le bonhomme est trop proche de l'obstacle et le faire disparaître
+    if(Math.abs(obstacleXPos - bonhommeXPos) - (bonhommeYPos - obstacleYPos) < 40){
+      bonhommeElement.style.color = "Blue";
     }
 }
 
 // Appeler la fonction de déplacement du bonhomme toutes les 100ms
-setInterval(moveBonhomme, 100);
+setInterval(moveElements, 100);
+
+// Fait monter le bonhomme
+function jump()
+{
+  bonhommeYPos -= 10;
+  bonhommeElement.style.top = bonhommeYPos + "px";
+}
+// Fait descendre le bonhomme
+function stepDown()
+{
+  bonhommeYPos += 10;
+  bonhommeElement.style.top = bonhommeYPos + "px";
+}
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Ne fait rien si l'évenement a déjà été effectué
+    }
+    // fait monter ou descendre le bonhomme suivant si on appuie sur la touche des flèches du bas ou du haut
+    switch (event.key) {
+      case "ArrowDown":
+        if(bonhommeYPos < 440)
+          {
+            stepDown()
+          }
+        break;
+      case "ArrowUp":        
+        jump()
+        break;
+      default:
+        return;
+    }
+    // Annule l'action par défaut pour éviter d'être traitée deux fois
+    event.preventDefault();
+  });
+  
