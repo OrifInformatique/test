adaf7943104d9bdb08c44bf7bf7cb2bcaabdd370
KarolJar <Karol.Jarzebowski@formation.orif.ch>
2025-09-19 15:57:26 +0200

    obstacle html game remaster

--- script.js
+++ script.js
@@ -9,11 +9,35 @@ let bonhommeYPos = 440; // Position initiale sur l'axe vertical
 
 let obstacleYPos = 450; // Position initiale sur l'axe vertical
 
+let grav = 0.5; // Gravité
+
+let xspd = 2; // Vitesse sur l'axe horizontal
+let yspd = 0; // Vitesse sur l'axe vertical
+
+let jumpSpd = 15; // Vitesse de saut
+let canJump = false
+
 function moveElements() {
-    bonhommeXPos += 10; // Déplace le bonhomme de 10 pixels à chaque fois
+
+    yspd += grav; // Applique la gravité à la vitesse vertical 
+  
+    
+
+    bonhommeXPos += xspd;
+    bonhommeYPos += yspd; // Met à jour la position verticale en fonction de la vitesse
+    bonhommeElement.style.top = bonhommeYPos + "px";
     bonhommeElement.style.left = bonhommeXPos + "px";
 
-    obstacleXPos -= 10; // Déplace l'obstacle de 10 pixels à chaque fois
+    if(bonhommeYPos > 440) // Empêche le bonhomme de tomber en dessous du sol
+    {
+      bonhommeYPos = 440;
+      canJump = true
+      yspd = 0;
+    }else{
+      canJump = false
+    }
+
+    obstacleXPos -= 2; // Déplace l'obstacle de 10 pixels à chaque fois
     obstacleElement.style.left = obstacleXPos + "px";
 
     // Réinitialise la position lorsque le bonhomme atteint la fin de la page
@@ -22,7 +46,7 @@ function moveElements() {
     }
     // Réinitialise la position lorsque l'obstacle atteint la fin de la page
     if (obstacleXPos < -100) {
-        obstacleXPos = 1800;
+        obstacleXPos = window.innerHeight;
     }
     // Calcule la différence de distance sur l'axe vertical et horizontal pour savoir si le bonhomme est trop proche de l'obstacle et le faire disparaître
     if(Math.abs(obstacleXPos - bonhommeXPos) - (bonhommeYPos - obstacleYPos) < 40){
@@ -31,19 +55,13 @@ function moveElements() {
 }
 
 // Appeler la fonction de déplacement du bonhomme toutes les 100ms
-setInterval(moveElements, 100);
+setInterval(moveElements, 10);
 
 // Fait monter le bonhomme
 function jump()
 {
-  bonhommeYPos -= 10;
-  bonhommeElement.style.top = bonhommeYPos + "px";
-}
-// Fait descendre le bonhomme
-function stepDown()
-{
-  bonhommeYPos += 10;
-  bonhommeElement.style.top = bonhommeYPos + "px";
+  if(!canJump) return
+  yspd = -jumpSpd;
 }
 
 window.addEventListener("keydown", function (event) {
@@ -52,12 +70,6 @@ window.addEventListener("keydown", function (event) {
     }
     // fait monter ou descendre le bonhomme suivant si on appuie sur la touche des flèches du bas ou du haut
     switch (event.key) {
-      case "ArrowDown":
-        if(bonhommeYPos < 440)
-          {
-            stepDown()
-          }
-        break;
       case "ArrowUp":        
         jump()
         break;
